df0d29d4321e53dea576b6de6fd7ba7eefe37d43
Daniel Pérez Solís <dpsol2008@gmail.com>
2020-06-01 14:36:46 -0500

    Initial Commit

---
+++ App2Less2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App2Less2
+//
+//  Created by Daniel Pérez Solís on 01/06/20.
+//  Copyright © 2020 Artnetsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
